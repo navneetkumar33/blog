80dd98ebec7ab0ce0f84f6f624d89b408ba20197
rajputnavneet33 <rajputnavneet87@gmail.com>
2023-06-01 11:49:47 +0530

    style 4

---
+++ src/component/Post.style.ts
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+export const StyledDivPost=styled.div`
+    height: 120px;
+    margin-top: 5px;
+    margin-bottom: 0px;
+ img{
+    float: left;
+ }
+ p{
+    flex-direction: column;
+    line-height: 25px;
+    text-align: start;
+    padding: 0px 25px;
+ }
+`
